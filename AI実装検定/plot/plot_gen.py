b846b010d7c9b7723e50b23eea42afc215616a04
QQDDTT <hunengwei0414@gmail.com>
2026-01-27 16:34:15 +0900

    20260127

--- AI実装検定/plot/plot_gen.py
+++ AI実装検定/plot/plot_gen.py
@@ -2,7 +2,12 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
 
-x = np.linspace(0, 10, 40)
-plt.plot(x, np.sin(x), marker='o', linestyle='-')
+plt.scatter(
+    np.random.randn(100),
+    np.random.randn(100),
+    c=np.random.randn(100),
+    s=np.random.randn(100) * 200,
+    alpha=0.5
+)
 
 plt.savefig('AI実装検定/plot/sin_3.png', dpi=300, bbox_inches='tight')
